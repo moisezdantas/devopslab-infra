9da2ccabeb51b649ef7f569877772c3b33c6a0be
Moisez Dantas <moises1695@gmail.com>
2021-05-15 12:26:47 -0300

    Convertendo Credential em Váriavel

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/google-cloud.json")
-
   project = "lavdevopscloud-moisez"
   region  = "us-central1"
   zone    = "us-central1-c"
